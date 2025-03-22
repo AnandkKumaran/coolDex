f7b7327d291670c6cdabe19985b90b7fc0429e05
Anand K <anandk.vly@gmail.com>
2025-03-22 21:47:22 +0530

    Add comments

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -1,10 +1,13 @@
-import React, {useState, useEffect} from 'react'
-import {Popover, Radio, Input, Modal} from "antd";
-import {SettingOutlined, DownOutlined, ArrowDownOutlined} from "@ant-design/icons";
+import React, { useState, useEffect } from "react";
+import { Popover, Radio, Input, Modal } from "antd";
+import {
+  SettingOutlined,
+  DownOutlined,
+  ArrowDownOutlined,
+} from "@ant-design/icons";
 import tokenList from "../tokenList.json";
 
 function Swap() {
-
   const [slippage, setSlippage] = useState(2.5);
 
   const [tokenOneAmount, setTokenOneAmount] = useState(null);
@@ -24,7 +27,7 @@ function Swap() {
     setTokenOneAmount(e.target.value);
   }
 
-  function changeTwoAmount(e){
+  function changeTwoAmount(e) {
     setTokenTwoAmount(e.target.value);
   }
 
@@ -41,8 +44,8 @@ function Swap() {
     setIsOpen(true);
   }
 
-  function modifyToken(i){
-    if(changeToken === 1){
+  function modifyToken(i) {
+    if (changeToken === 1) {
       setTokenOne(tokenList[i]);
     } else {
       setTokenTwo(tokenList[i]);
@@ -60,70 +63,92 @@ function Swap() {
           <Radio.Button value={5}>5%</Radio.Button>
         </Radio.Group>
       </div>
-    </>     
-  )
+    </>
+  );
 
   return (
     <>
+      {/* Coin List Modal */}
       <Modal
         open={isOpen}
         footer={null}
-        onCancel={()=> setIsOpen(false)}
+        onCancel={() => setIsOpen(false)}
         title="Select a Token"
       >
-        <div className='modalContent'>
-          {
-            tokenList?.map((e,i)=>{
-              return(
-                <div
-                  className='tokenChoice'
-                  key={i}
-                  onClick={()=>modifyToken(i)}
-                >
-                  <img src={e.img} alt={e.ticker} className='tokenLogo'/>
-                  <div className='tokenChoiceNames'>
-                    <div className='tokenName'>{e.name}</div>
-                    <div className='tokenTicker'>{e.ticker}</div>
-                  </div>
+        <div className="modalContent">
+          {tokenList?.map((e, i) => {
+            return (
+              <div
+                className="tokenChoice"
+                key={i}
+                onClick={() => modifyToken(i)}
+              >
+                <img src={e.img} alt={e.ticker} className="tokenLogo" />
+                <div className="tokenChoiceNames">
+                  <div className="tokenName">{e.name}</div>
+                  <div className="tokenTicker">{e.ticker}</div>
                 </div>
-              );
-            })
-          }
+              </div>
+            );
+          })}
         </div>
       </Modal>
-      <div className='tradeBox'>
-        <div className='tradeBoxHeader'>
+
+      {/* Trade box div for swap */}
+      <div className="tradeBox">
+        <div className="tradeBoxHeader">
           <h4>Swap</h4>
           <Popover
             content={settings}
             title="Settings"
             trigger="click"
-            placement='bottomRight'
+            placement="bottomRight"
           >
-            <SettingOutlined className="cog"/>
+            <SettingOutlined className="cog" />
           </Popover>
         </div>
-        <div className='inputs'>
-          <Input placeholder='0' value={tokenOneAmount} onChange={changeOneAmount}/>
-          <Input placeholder='0' value={tokenTwoAmount} onChange={changeTwoAmount} disabled={true}/>
-          <div className='assetOne' onClick={() => openModal(1)}>
-            <img src={tokenOne.img} alt='assetOneLogo' className='assetLogo'/>
+
+        {/* Input Boxes */}
+        <div className="inputs">
+          <Input
+            placeholder="0"
+            value={tokenOneAmount}
+            onChange={changeOneAmount}
+          />
+          <Input
+            placeholder="0"
+            value={tokenTwoAmount}
+            onChange={changeTwoAmount}
+            disabled={true}
+          />
+
+          {/* Coin 1 selection */}
+          <div className="assetOne" onClick={() => openModal(1)}>
+            <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
             {tokenOne.ticker}
-            <DownOutlined/>
+            <DownOutlined />
           </div>
-          <div className='switchButton' onClick={switchTokens}>
-            <ArrowDownOutlined className='switchArrow'/>
+
+          {/* Coin Switching */}
+          <div className="switchButton" onClick={switchTokens}>
+            <ArrowDownOutlined className="switchArrow" />
           </div>
-          <div className='assetTwo' onClick={() => openModal(2)}>
-            <img src={tokenTwo.img} alt='assetTwoLogo' className='assetLogo'/>
-              {tokenTwo.ticker}
-            <DownOutlined/>
+
+          {/* Coin 2 selection */}
+          <div className="assetTwo" onClick={() => openModal(2)}>
+            <img src={tokenTwo.img} alt="assetTwoLogo" className="assetLogo" />
+            {tokenTwo.ticker}
+            <DownOutlined />
           </div>
         </div>
-        <div className='swapButton' disabled={!tokenOneAmount}>Swap</div>
+
+        {/* Swap button */}
+        <div className="swapButton" disabled={!tokenOneAmount}>
+          Swap
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Swap
+export default Swap;

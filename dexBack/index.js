34450afa22d0c8c8c731a041b7d82c5dde912553
Anand K <anandk.vly@gmail.com>
2025-03-28 02:04:34 +0530

    Add Moralis API in backend to fetch token prices

--- dexBack/index.js
+++ dexBack/index.js
@@ -9,7 +9,37 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/tokenPrice", async (req, res) => {
-  return res.status(200).json(usdPrices);
+  try {
+    const { addressTokenOne, addressTokenTwo } = req.query;
+
+    // Sanitize
+    if (!addressTokenOne || !addressTokenTwo) {
+      return res.status(400).json({ error: "No address provided" });
+    }
+
+    // TODO Validate addresses
+
+    const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+      address: addressTokenOne,
+    });
+
+    const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+      address: addressTokenTwo,
+    });
+
+    console.log(responseOne);
+
+    const usdPrices = {
+      tokenOne: responseOne.raw.usdPrice,
+      tokenTwo: responseTwo.raw.usdPrice,
+      ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice,
+    };
+
+    return res.status(200).json(usdPrices);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
 });
 
 Moralis.start({
